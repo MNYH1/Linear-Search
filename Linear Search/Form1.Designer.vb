dc077a0a51a6d44bdf08a3b07029e21816db584e
HMN <HMN@STAFF-6177>
2022-06-20 13:51:20 +1000

    Add project files.

---
+++ Linear Search/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGen = New System.Windows.Forms.Button()
+        Me.btnLinearSearch = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnGen
+        '
+        Me.btnGen.Location = New System.Drawing.Point(20, 37)
+        Me.btnGen.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnGen.Name = "btnGen"
+        Me.btnGen.Size = New System.Drawing.Size(97, 37)
+        Me.btnGen.TabIndex = 0
+        Me.btnGen.Text = "Generate"
+        Me.btnGen.UseVisualStyleBackColor = True
+        '
+        'btnLinearSearch
+        '
+        Me.btnLinearSearch.Location = New System.Drawing.Point(20, 93)
+        Me.btnLinearSearch.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnLinearSearch.Name = "btnLinearSearch"
+        Me.btnLinearSearch.Size = New System.Drawing.Size(97, 37)
+        Me.btnLinearSearch.TabIndex = 1
+        Me.btnLinearSearch.Text = "Linear Search"
+        Me.btnLinearSearch.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(20, 203)
+        Me.btnQuit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(97, 37)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "End"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(20, 146)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(97, 37)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear List"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(145, 40)
+        Me.lstDisplay.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(139, 303)
+        Me.lstDisplay.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 377)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnLinearSearch)
+        Me.Controls.Add(Me.btnGen)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGen As Button
+    Friend WithEvents btnLinearSearch As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ Linear Search/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Const MAXINDEX As Integer = 20
+    Dim numbers(MAXINDEX) As Integer
+
+    Dim itemCount As Integer = 0
+
+    Private Sub btnGen_Click(sender As Object, e As EventArgs) Handles btnGen.Click
+        Randomize()
+
+        Dim i As Integer
+
+        'clear display box
+        lstDisplay.Items.Clear()
+
+        'Dim letter As String = ""
+        'Dim rand As New Random
+        'For i = 0 To MAXINDEX
+        '    letter = letter & ChrW(rand.Next(Asc("A"), Asc("Z") + 1))
+        'Next
+
+        'lstDisplay.Items.Add(letter)
+
+
+        For i = 0 To MAXINDEX 'loop 20 times
+            numbers(i) = CInt(500 * Rnd() + 1) 'pick a random number and store it in an array
+            lstDisplay.Items.Add(numbers(i)) 'display the random number in the list box
+            itemCount += 1
+        Next 'increment value of loops
+        btnGen.Enabled = False
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        lstDisplay.Items.Clear()
+        itemCount = 0
+        btnGen.Enabled = True
+
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        End
+
+    End Sub
+
+    Private Sub btnLinearSearch_Click(sender As Object, e As EventArgs) Handles btnLinearSearch.Click
+        Dim itemToFind As Integer
+        'Dim numComparison As Integer   'used for my search
+        Dim subFindIndex As Integer
+
+        itemToFind = CInt(InputBox("What do you want to look for?", "Search"))
+
+        'calls linear search function
+        'subFindIndex = linearSearch(itemToFind, numComparison) '***used for my search to locate the number of comparisons
+        subFindIndex = linearSearch(itemToFind)
+
+        If subFindIndex > -1 Then
+            MsgBox(itemToFind & " was found at position " & subFindIndex)   'for Jansons Search
+            'MsgBox(itemToFind & " was found at position " & subFindIndex & " using " & numComparison & " comparisons", , "Found")    'For my search
+        Else
+            MsgBox(itemToFind & " was not found in the list", , "Not Found")
+        End If
+    End Sub
+#Region "linear search function"
+    'Function LinearSearch(ByVal item As Integer, ByRef numComparison As Integer) As Integer    '***used for my search number of comparisons
+    '    Dim i As Integer
+    '    Dim max As Integer
+    '    Dim subFindItemIndex As Integer
+
+    '    i = 0
+    '    max = itemCount - 1 'initialize max to number of items in the array
+    '    subFindItemIndex = -1   'initialize item to not found
+    '    numComparison = 0   'initialize number of comparisons
+
+    '    While i <= max And subFindItemIndex = -1
+    '        numComparison += 1
+    '        If numbers(i) = item Then
+    '            subFindItemIndex = i    'set subFindIndex to index of found element
+    '        End If
+    '        i += 1
+    '    End While
+
+    '    Return subFindItemIndex
+
+    'End Function
+#End Region
+
+    Function linearSearch(ByVal item As Integer) As Integer
+        Dim i As Integer = 0
+        Dim max As Integer
+        Dim subFindItemIndex As Integer
+
+        max = itemCount - 1
+        subFindItemIndex = -1
+
+        While i <= max And subFindItemIndex = -1
+            If numbers(i) = item Then
+                subFindItemIndex = i
+            End If
+            i += 1
+        End While
+        Return subFindItemIndex
+    End Function
+End Class
